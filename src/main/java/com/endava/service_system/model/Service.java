e146db22b211dda47e348cc240189ebbdafd1845
vcumpana <victorcumpana@gmail.com>
2018-04-23 11:40:55 +0300

    add company entity ,dao ,service

---
+++ src/main/java/com/endava/service_system/model/Service.java
@@ -0,0 +1,17 @@
+package com.endava.service_system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+}
